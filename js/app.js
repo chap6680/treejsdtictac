56edccdea16427cd76d45922d99b7621d2e72585
david chapman <chap6680@gmail.com>
2018-05-31 08:03:26 -0600

    submit thurs updates with history tracking

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
+
+/*  Declare variables player1 is 0 and player2 is 1
+*/
 const playerNames = ['player1', 'player2'];
 let currentPlayer = 0;
 let nextPlayer = 1;
 let numberOfPlayers = 1;
 let countEmpties = 0;
 let emptyBoxes = new Array;
+let tempUndefined = new Array;
+let winner = '';
+
 let playerInfo = new Array;
 playerInfo = [{
 		setActive: 'player1',
 		boxFill: 'box-filled-1',
 		boxhover: 'hover1',
 		winnerScreen: 'screen-win-one',
-	  	pname: 'Player 1'
+		pname: 'Player 1'
 	},
 	{
 		setActive: 'player2',
@@ -20,12 +26,37 @@ playerInfo = [{
 		pname: 'Computer'
 	}
 ]
-let gridTrack = new Array;
-let gridTrackHistory;
 
-//let gridTrackHistory = 0;
-gridTrack[gridTrackHistory] =new Array;
-let winners = [
+/* gridTrack is an Array that is tracking the game.
+
+as players fillout the grid, a 0 or 1 is placed into the array, using [1] through [9]
+
+Multiple games - 
+Added history tracking - this array adds every game as a separate array within gridTrack.  Allows for tracking...future enhancements such as displaying the history on the side.
+Played with storing this into local storage */
+let gridTrack = new Array;
+let gridTrackGameNo = 0;
+gridTrack[gridTrackGameNo] = new Array;
+
+/*
+LSgridTrack = JSON.parse(localStorage.getItem("treetictac"));
+gridTrack = LSgridTrack;
+console.log('ls: ',LSgridTrack);
+ if  (LSgridTrack === 'undefined' || LSgridTrack===null) {
+	gridTrackGameNo = 0;
+} else {
+	gridTrackGameNo = LSgridTrack.length;
+	getTrack = LSgridTrack;
+	console.log('setArray:', getTrack);
+ } 
+gridTrack.length = 0;
+let gridTrackGameNo = 0;
+*/
+
+//winningNumbers is an array of winning solutions.
+//using grid, starting with 1 2 3, then 4, 5, 6
+//these are the "locations" within the grid that are winners
+let winningNumbers = [
 	[1, 2, 3],
 	[4, 5, 6],
 	[7, 8, 9],
@@ -35,104 +66,112 @@ let winners = [
 	[1, 5, 9],
 	[3, 5, 7]
 ];
-/* Adds Element AFTER NeighborElement */
-Element.prototype.appendAfter = function (element) {
-	element.parentNode.insertBefore(this, element.nextSibling);
-}, false;
 
+
+/* checks to see if anyone won - does this by going through the winningNumbers Array and seeing it it matches all 0s or 1s within the gridTrack*/
 function checkWinners() {
 	for (let checkvar = 0; checkvar < 8; checkvar++) {
-		for (let checkvarPlayer = 0; checkvarPlayer <= 1; checkvarPlayer++) {
-			//console.log('checking w');
-			if (gridTrack[gridTrackHistory][winners[checkvar][0]] === checkvarPlayer && gridTrack[gridTrackHistory][winners[checkvar][1]] === checkvarPlayer && gridTrack[gridTrackHistory][winners[checkvar][2]] === checkvarPlayer) {
-				//console.log('winner player', checkvarPlayer);
-				gridTrackHistory += 1;
-				
-				localStorage.setItem('treetictac', JSON.stringify(gridTrack));	
-				insertWinnerScreen(checkvarPlayer);
-			}
+			for (let checkvarPlayer = 0; checkvarPlayer <= 1; checkvarPlayer++) {
+			
+				if (gridTrack[gridTrackGameNo][winningNumbers[checkvar][0]] === checkvarPlayer && gridTrack[gridTrackGameNo][winningNumbers[checkvar][1]] === checkvarPlayer && gridTrack[gridTrackGameNo][winningNumbers[checkvar][2]] === checkvarPlayer) {
+					gridTrackGameNo += 1;
+
+					//	localStorage.setItem('treetictac', JSON.stringify(gridTrack));
+					winner = checkvarPlayer;
+					gridTrack[gridTrackGameNo-1][10] = "Winner: " + playerInfo[checkvarPlayer].pname;
+					return insertWinnerScreen(checkvarPlayer);
+				}
 		}
 	}
+
+	//if the entire grid is filled out, needs to display a tie screen
+	//definitely a cleaner way to do this, but it works.
 	let countEmpties = 1;
 	for (let t = 1; t < 10; t++) {
-		if (gridTrack[gridTrackHistory][t] === 1 || gridTrack[gridTrackHistory][t] === 0) {
-			
-		} else { 
+		if (gridTrack[gridTrackGameNo][t] === 1 || gridTrack[gridTrackGameNo][t] === 0) {
+		} else {
 			countEmpties = 0;
 		}
-		//	console.log("no players:", numberOfPlayers, ' - current P ', currentPlayer);
 	}
 	if (countEmpties === 1) {
-		gridTrackHistory += 1;
+		gridTrackGameNo += 1;
+		winner = 'tie';
+		gridTrack[gridTrackGameNo-1][10] = "Tie";
 		insertWinnerScreen('tie')
 	};
 }
 
-function computerPick() { 
+//If one player the computer is randomly picking out a square.
+function computerPick() {
 	emptyBoxes.length = 0;
-	for (let t = 1; t < 10; t++) { 
-		if (gridTrack[gridTrackHistory][t] === 1 || gridTrack[gridTrackHistory][t] === 0) {
-
-		} else { 
-
+	for (let t = 1; t < 10; t++) {
+		if (gridTrack[gridTrackGameNo][t] === 1 || gridTrack[gridTrackGameNo][t] === 0) {} else {
 			emptyBoxes.push(t);
 		}
 	}
-	getComputerPick = getRandomInt(emptyBoxes.length);	
+	getComputerPick = getRandomInt(emptyBoxes.length);
 	getComputerPickIndex = emptyBoxes[getComputerPick];
-//	console.log(' box is: ', 'box'+getComputerPickIndex, 'currentPlayer: ', currentPlayer);	
 	setBox('box' + getComputerPickIndex, currentPlayer);
 }
 
 function getRandomInt(max) {
 	return Math.floor(Math.random() * Math.floor(max));
-  }
-  
+}
 
-function btnChange() { 
+
+function btnChange() {
 	document.getElementById('finish').style.display = 'none';
 	document.getElementById('start').style.display = 'block';
 }
 
-function btnStart() {
+function btnHistory() {
 	
+	let tempHistory = '';
+	let tempHistoryNo = 0;
+	Array.from(gridTrack).forEach(function (x) {
+		tempHistoryNo = tempHistoryNo + 1;
+		tempHistory =  tempHistory + '\n' + 'Game ' + tempHistoryNo + ' ' + x[0] + ' : ' + x[10];
+	});
+	alert(tempHistory);
+}
+
+
+function btnStart() {
+	winner = '';
 	document.getElementById('start').style.display = 'none';
 	document.getElementById('finish').style.display = 'none';
 	updateName();
 	emptyBoxes[0] = 1;
-	console.log('current game number is: '+ gridTrackHistory);
-	if (gridTrackHistory > 0) { 
-//		gridTrack[gridTrackHistory].length = 0;
+	if (gridTrackGameNo > 0) {
 		for (let i = 1; i <= 9; i++) {
 			let tempx = "box" + i;
 			document.getElementById('box' + i).classList.remove('box-filled-1');
 			document.getElementById('box' + i).classList.remove('box-filled-2');
 			document.getElementById('box' + i).classList.remove('hover2');
 		}
-		document.getElementById('finish').classList.remove( playerInfo[0].winnerScreen);
+		document.getElementById('finish').classList.remove(playerInfo[0].winnerScreen);
 		document.getElementById('finish').classList.remove(playerInfo[1].winnerScreen);
 		document.getElementById('finish').classList.remove('screen-win-tie');
-	
+
 	}
 	currentPlayer = 0;
-	gridTrack[gridTrackHistory] = new Array;
-	gridTrack[gridTrackHistory][0] =  (playerInfo[0].pname + ' - ' + playerInfo[1].pname);
-	console.log('array ', gridTrack[gridTrackHistory][0]);
-
+	gridTrack[gridTrackGameNo] = new Array;
+	gridTrack[gridTrackGameNo][0] = (playerInfo[0].pname + ' - ' + playerInfo[1].pname);
+	
 	switchActivePlayer(currentPlayer);
-	//switchActivePlayer(currentPlayer);
 }
 
-function updateName() { 
+//puts the player name in the header box
+function updateName() {
 	playerInfo[0].pname = document.getElementById('player1name').value;
 	playerInfo[1].pname = document.getElementById('player2name').value;
 	document.getElementById('player1').children[1].textContent = playerInfo[0].pname;
 	if (numberOfPlayers == 2) {
 		document.getElementById('player2').children[1].textContent = playerInfo[1].pname;
-	}	
+	}
 }
 
-function btnOnePlayer() { 
+function btnOnePlayer() {
 	document.getElementById('btnPlayerNum2').classList.remove('active');
 	document.getElementById('btnPlayerNum1').classList.add('active');
 	document.getElementById('player2group').style.display = 'none';
@@ -141,37 +180,42 @@ function btnOnePlayer() {
 	numberOfPlayers = 1;
 
 }
-function btnTwoPlayer() { 
+
+function btnTwoPlayer() {
 	document.getElementById('btnPlayerNum1').classList.remove('active');
 	document.getElementById('btnPlayerNum2').classList.add('active');
 	document.getElementById('player2group').style.display = 'block';
 	numberOfPlayers = 2;
+	if (document.getElementById('player2name').value = "Computer") { 
+		document.getElementById('player2name').value = 'Player 2';
+	};
 
 	let getC = document.getElementById('player1').getElementsByTagName('p');
-	
-	console.log(getC);
-	document.getElementById('player1').children[1].textContent = document.getElementById('player1name').value ;
+
+	document.getElementById('player1').children[1].textContent = document.getElementById('player1name').value;
 }
 
-
-
+//inserts winning screen or tie screen
 function insertWinnerScreen(player) {
 	document.getElementById('finish').style.display = 'block';
 	let tempE = document.getElementById('finish').children[2];
 	if (player == 'tie') {
-		console.log('TIE ', tempE);
 		let tempEE = "TIE";
 		document.getElementById('finish').children[2].innerHTML = tempEE;
-			document.getElementById('finish').classList.add('screen-win-tie')
-	} else { 
-	console.log('winner ', tempE);
-	let tempEE = "Winner<br>" + playerInfo[player].pname;
-	document.getElementById('finish').children[2].innerHTML = tempEE;
-		document.getElementById('finish').classList.add(playerInfo[currentPlayer].winnerScreen)
+		document.getElementById('finish').classList.add('screen-win-tie');
+		document.getElementById(playerInfo[0].setActive).className = 'players';
+		document.getElementById(playerInfo[1].setActive).className = 'players';
+	} else {
+		let tempEE = "Winner<br>" + playerInfo[player].pname;
+		document.getElementById('finish').children[2].innerHTML = tempEE;
+		
+		document.getElementById('finish').classList.add(playerInfo[currentPlayer].winnerScreen);
+		document.getElementById(playerInfo[Math.abs(player)].setActive).className='players';
 	};
 
 }
 
+//function called after every play - to switch current player
 function switchActivePlayer(getPlayer) {
 	let tempActiveClass = 'players active';
 
@@ -179,86 +223,56 @@ function switchActivePlayer(getPlayer) {
 	let setOtherPlayer = document.getElementById(playerInfo[nextPlayer].setActive).className = 'players';
 }
 
+
+//Grid - filling in the X and O selected
+//boxloc is the gridbox number, cp is current player (1 or 0)
 function setBox(boxloc, cp) {
-	console.log('box', boxloc, " ..", currentPlayer);
-
 	let checkstring = boxloc.substr(boxloc.length - 1);
-//	console.log('check gridplacement:', checkstring, gridTrack[gridTrackHistory][checkstring]);
-
-	if (gridTrack[gridTrackHistory][checkstring] === 0 || gridTrack[gridTrackHistory][checkstring] === 1) {
+	
+	if (gridTrack[gridTrackGameNo][checkstring] === 0 || gridTrack[gridTrackGameNo][checkstring] === 1) {
 		return;
 	};
-
 
 	document.getElementById(boxloc).classList.add(playerInfo[cp].boxFill);
 
 	//set gridTrack Array with selection
-	gridTrack[gridTrackHistory][boxloc.substr(boxloc.length - 1)] = cp;
-
+	gridTrack[gridTrackGameNo][boxloc.substr(boxloc.length - 1)] = cp;
 
 	if (currentPlayer == 0) {
 		for (let i = 1; i <= 9; i++) {
-			console.log('forloo in setbox:',i,gridTrack[gridTrackHistory][i]);
-			if (gridTrack[gridTrackHistory][i] === 1 || gridTrack[gridTrackHistory][i] === ''|| gridTrack[gridTrackHistory][i]===undefined) {
-				console.log('next inside', i);
+			if (gridTrack[gridTrackGameNo][i] === 1 || gridTrack[gridTrackGameNo][i] === '' || gridTrack[gridTrackGameNo][i] === undefined) {
 				document.getElementById('box' + i).classList.add('hover2');
 			}
 		}
 	} else {
 		let hover2boxes = document.getElementsByClassName('hover2');
-		console.log('h2box:', hover2boxes);
 		Array.from(hover2boxes).forEach(function (x) {
 			x.classList.remove("hover2");
 		});
 	}
 
 	checkWinners();
-	document.getElementById(playerInfo[cp].setActive).classList.remove('active');
-	document.getElementById(playerInfo[Math.abs(cp - 1)].setActive).classList.add('active');
-
+	if (winner !== 'tie') {
+		document.getElementById(playerInfo[cp].setActive).classList.remove('active');
+	//	document.getElementById(playerInfo[Math.abs(cp - 1)].setActive).classList.add('active');
+	};
 
 	currentPlayer = Math.abs(cp - 1);
 
-	if (numberOfPlayers === 1 && currentPlayer===1) {
+	if (numberOfPlayers === 1 && currentPlayer === 1&&winner==='') {
 		computerPick();
-	 }
-
+	}
 
 }
 
+
+//set eventListener on setup for the grid
 for (let i = 1; i <= 9; i++) {
 	let tempx = "box" + i;
-	//console.log(tempx);
-	
-	gridTrack[gridTrackHistory][i] = '';
+	gridTrack[gridTrackGameNo][i] = '';
 	document.addEventListener('DOMContentLoaded', function () {
 		document.getElementById(tempx).addEventListener('click', function () {
 			setBox(tempx, currentPlayer)
 		})
 	});
 }
-
-function storageAvailable(type) {
-    try {
-        var storage = window[type],
-            x = '__storage_test__';
-        storage.setItem(x, x);
-        storage.removeItem(x);
-        return true;
-    }
-    catch(e) {
-        return e instanceof DOMException && (
-            // everything except Firefox
-            e.code === 22 ||
-            // Firefox
-            e.code === 1014 ||
-            // test name field too, because code might not be present
-            // everything except Firefox
-            e.name === 'QuotaExceededError' ||
-            // Firefox
-            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-            // acknowledge QuotaExceededError only if there's something already stored
-            storage.length !== 0;
-    }
-} 
-
